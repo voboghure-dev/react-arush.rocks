f53c56eb328e9544225f2c8e95f256080d2b50ab
Tapan Kumer Das <voboghure.dev@gmail.com>
2022-01-23 23:53:24 +0600

    Add material icon and fix topbar left part
    Signed-off-by: Tapan Kumer Das <voboghure.dev@gmail.com>

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,15 +1,24 @@
 import './topbar.scss';
+import { Person, Mail } from '@material-ui/icons';
 
 export default function Topbar() {
   return (
     <div className='topbar'>
       <div className='wrapper'>
         <div className='left'>
-          <a href='#intro' className='logo'>Logo</a>
+          <a href='#intro' className='logo'>
+            Logo
+          </a>
+          <div className='itemContainer'>
+            <Person className='icon' />
+            <span>+88-01973-238664</span>
+          </div>
+          <div className='itemContainer'>
+            <Mail className='icon' />
+            <span>voboghure.dev@gmail.com</span>
+          </div>
         </div>
-        <div className='right'>
-          right content
-        </div>
+        <div className='right'>right content</div>
       </div>
     </div>
   );
