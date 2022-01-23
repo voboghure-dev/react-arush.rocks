1968a255493722e77254916b71561124a15019c6
Tapan Kumer Das <voboghure.dev@gmail.com>
2022-01-24 01:02:14 +0600

    Add hamburger menu
    Signed-off-by: Tapan Kumer Das <voboghure.dev@gmail.com>

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,9 +1,9 @@
 import './topbar.scss';
 import { Person, Mail } from '@material-ui/icons';
 
-export default function Topbar() {
+export default function Topbar({ menuOpen, setMenuOpen }) {
   return (
-    <div className='topbar'>
+    <div className={'topbar ' + (menuOpen && 'active')}>
       <div className='wrapper'>
         <div className='left'>
           <a href='#intro' className='logo'>
@@ -18,7 +18,13 @@ export default function Topbar() {
             <span>voboghure.dev@gmail.com</span>
           </div>
         </div>
-        <div className='right'>right content</div>
+        <div className='right'>
+          <div className='hamburger' onClick={() => setMenuOpen(!menuOpen)}>
+            <span className='line1'></span>
+            <span className='line2'></span>
+            <span className='line3'></span>
+          </div>
+        </div>
       </div>
     </div>
   );
