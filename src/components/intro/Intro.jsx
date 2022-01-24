77e51a2256a1296ea93d8bc02d2e10258c936b94
Tapan Kumer Das <voboghure.dev@gmail.com>
2022-01-24 13:55:29 +0600

    Add ityped library and finish Intro
    Signed-off-by: Tapan Kumer Das <voboghure.dev@gmail.com>

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,20 +1,34 @@
 import './intro.scss';
+import { init } from 'ityped';
+import { useEffect, useRef } from 'react';
 
 export default function Intro() {
+  const textRef = useRef();
+
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: true,
+      strings: ['Agile', 'Restless', 'Curious'],
+    });
+  }, []);
+
   return (
     <div className='intro' id='intro'>
       <div className='left'>
-        <div className="imgContainer">
-          <img src="assets/arush.png" alt="Arush" />
+        <div className='imgContainer'>
+          <img src='assets/arush.png' alt='Arush' />
         </div>
       </div>
       <div className='right'>
-        <div className="wrapper">
+        <div className='wrapper'>
           <h2>Hi There, I'm</h2>
           <h1>Ishan Das (Arush)</h1>
+          <h3>
+            Too much <span ref={textRef}></span>
+          </h3>
         </div>
-        <a href="#portfolio">
-          <img src="assets/down.png" alt="" />
+        <a href='#portfolio'>
+          <img src='assets/down.png' alt='' />
         </a>
       </div>
     </div>
