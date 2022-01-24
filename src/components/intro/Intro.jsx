8d405609bec9cf074a69e617c6ccd047916d4c7f
Tapan Kumer Das <voboghure.dev@gmail.com>
2022-01-24 13:24:11 +0600

    Build Intro section
    Signed-off-by: Tapan Kumer Das <voboghure.dev@gmail.com>

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,5 +1,22 @@
-import './intro.scss'
+import './intro.scss';
 
 export default function Intro() {
-  return <div className='intro' id='intro'>Intro</div>;
+  return (
+    <div className='intro' id='intro'>
+      <div className='left'>
+        <div className="imgContainer">
+          <img src="assets/arush.png" alt="Arush" />
+        </div>
+      </div>
+      <div className='right'>
+        <div className="wrapper">
+          <h2>Hi There, I'm</h2>
+          <h1>Ishan Das (Arush)</h1>
+        </div>
+        <a href="#portfolio">
+          <img src="assets/down.png" alt="" />
+        </a>
+      </div>
+    </div>
+  );
 }
