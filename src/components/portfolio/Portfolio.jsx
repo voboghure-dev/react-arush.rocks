49d2f6d9854b51458ecaaad8c27c94eaf6e83ce7
Tapan Kumer Das <voboghure.dev@gmail.com>
2022-01-25 00:07:00 +0600

    Use dynamic data with useEffect in Portfolio page
    Signed-off-by: Tapan Kumer Das <voboghure.dev@gmail.com>

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,17 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import PortfolioList from '../portfolioList/PortfolioList';
 import './portfolio.scss';
+import {
+  featuredPortfolio,
+  webPortfolio,
+  mobilePortfolio,
+  designPortfolio,
+  contentPortfolio,
+} from '../../data';
 
 export default function Portfolio() {
   const [selected, setSelected] = useState('featured');
+  const [data, setData] = useState([]);
   const list = [
     {
       id: 'featured',
@@ -26,6 +34,28 @@ export default function Portfolio() {
       title: 'Content',
     },
   ];
+
+  useEffect(() => {
+    switch (selected) {
+      case 'featured':
+        setData(featuredPortfolio);
+        break;
+      case 'web':
+        setData(webPortfolio);
+        break;
+      case 'mobile':
+        setData(mobilePortfolio);
+        break;
+      case 'design':
+        setData(designPortfolio);
+        break;
+      case 'content':
+        setData(contentPortfolio);
+        break;
+      default:
+        setData(featuredPortfolio);
+    }
+  }, [selected]);
   return (
     <div className='portfolio' id='portfolio'>
       <h1>Portfolio</h1>
@@ -40,55 +70,12 @@ export default function Portfolio() {
         ))}
       </ul>
       <div className='container'>
-        <div className='item'>
-          <img
-            src='https://images.unsplash.com/photo-1523839699072-5ec088b61a21?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=250&q=80'
-            alt=''
-          />
-          <h3>Banking App</h3>
-        </div>
-        <div className='item'>
-          <img
-            src='https://images.unsplash.com/photo-1523839699072-5ec088b61a21?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=250&q=80'
-            alt=''
-          />
-          <h3>Banking App</h3>
-        </div>
-        <div className='item'>
-          <img
-            src='https://images.unsplash.com/photo-1523839699072-5ec088b61a21?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=250&q=80'
-            alt=''
-          />
-          <h3>Banking App</h3>
-        </div>
-        <div className='item'>
-          <img
-            src='https://images.unsplash.com/photo-1523839699072-5ec088b61a21?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=250&q=80'
-            alt=''
-          />
-          <h3>Banking App</h3>
-        </div>
-        <div className='item'>
-          <img
-            src='https://images.unsplash.com/photo-1523839699072-5ec088b61a21?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=250&q=80'
-            alt=''
-          />
-          <h3>Banking App</h3>
-        </div>
-        <div className='item'>
-          <img
-            src='https://images.unsplash.com/photo-1523839699072-5ec088b61a21?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=250&q=80'
-            alt=''
-          />
-          <h3>Banking App</h3>
-        </div>
-        <div className='item'>
-          <img
-            src='https://images.unsplash.com/photo-1523839699072-5ec088b61a21?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=250&q=80'
-            alt=''
-          />
-          <h3>Banking App</h3>
-        </div>
+        {data.map((d) => (
+          <div className='item'>
+            <img src={d.img} alt='' />
+            <h3>{d.title}</h3>
+          </div>
+        ))}
       </div>
     </div>
   );
