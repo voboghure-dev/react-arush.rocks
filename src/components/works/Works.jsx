f1007f49711a45259bca49fd37f4c31a8132dece
Tapan Kumer Das <voboghure.dev@gmail.com>
2022-01-25 01:16:03 +0600

    Build dummy slider container for works
    Signed-off-by: Tapan Kumer Das <voboghure.dev@gmail.com>

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -1,5 +1,30 @@
 import './works.scss';
 
 export default function Works() {
-  return <div className='works' id='works'>Works</div>;
+  return (
+    <div className='works' id='works'>
+      <div className='slider'>
+        <div className='container'>
+          <div className='item'>
+            <div className='left'>
+              <div className='leftContainer'>
+                <div className='imgContainer'>
+                  <img src='assets/mobile.png' alt='' />
+                </div>
+                <h2>Title</h2>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse,
+                  obcaecati quod error neque asperiores.
+                </p>
+                <span>Projects</span>
+              </div>
+            </div>
+            <div className='right'>
+              <img src="https://99designs-blog.imgix.net/blog/wp-content/uploads/2018/10/attachment_100040756-e1538485934255.jpeg?auto=format&q=60&fit=max&w=930" alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
