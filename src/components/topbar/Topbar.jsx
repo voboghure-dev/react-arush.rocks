92391feeecf2af19fd75da2dd89f785a7dfde777
Tapan Kumer Das <voboghure.dev@gmail.com>
2022-01-25 11:54:16 +0600

    Build Works slider
    Signed-off-by: Tapan Kumer Das <voboghure.dev@gmail.com>

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -7,7 +7,7 @@ export default function Topbar({ menuOpen, setMenuOpen }) {
       <div className='wrapper'>
         <div className='left'>
           <a href='#intro' className='logo'>
-            Logo
+            Arush.rocks
           </a>
           <div className='itemContainer'>
             <Person className='icon' />
